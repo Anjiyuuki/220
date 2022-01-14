06d7182ac80f0689f40dad97edd236383725ceaa
Angie Bui <Buia@g.cofc.edu>
2022-01-14 14:30:56 -0500

    notes

--- notes/Chapter 1 Notes.py
+++ notes/Chapter 1 Notes.py
@@ -1,5 +1,9 @@
-
-
+"""
+Notes:
+eval means exact number
+def variable():
+    variable= eval(input(prompt))
+"""
 def convert():
     celsius = eval(input("Enter temp in celsius:"))
     fahrenheit= celsius * 9/5 +32
