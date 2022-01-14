bd9d81e7986949e67a7ca624558b854e5affd1d8
Angie Bui <Buia@g.cofc.edu>
2022-01-14 15:38:15 -0500

    HW1

--- notes/Chapter 1 Notes.py
+++ notes/Chapter 1 Notes.py
@@ -5,5 +5,5 @@ def variable():
 """
 def convert():
     celsius = eval(input("Enter temp in celsius:"))
-    fahrenheit= celsius * 9/5 +32
+    fahrenheit = celsius * 9/5 +32
     print(fahrenheit)
