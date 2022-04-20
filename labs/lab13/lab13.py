6ded2ffbcb6212358d16538a5c93a0cbf869773d
Anjiyuuki <95329023+Anjiyuuki@users.noreply.github.com>
2022-04-19 22:37:52 -0400

    Update lab13.py

---
+++ labs/lab13/lab13.py
@@ -0,0 +1,24 @@
+"""
+Name: Angie Bui
+lab13.py
+
+Problem: Option 1 Capstone, trade alert
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def trade_alert(file_name):
+    in_file = open(file_name, "r")
+    line = in_file.read()
+    word = line.split()
+    for num in range(len(word)):
+        if eval(word[num]) > 830:
+            location = num + 1
+            print("Warning! Volume exceeds 830 at " + str(location) + " seconds!")
+        elif eval(word[num]) == 500:
+            location = num + 1
+            print("Pay attention! Volume is equal to 500 at " + str(location) + " seconds!")
+    in_file.close()
+
